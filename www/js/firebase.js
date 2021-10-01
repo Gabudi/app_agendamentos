301abc6fa6162537d380cf11ea512e68e8cee4a3
Gabudi <gabrielfernandes0609@gmail.com>
2021-09-30 22:56:45 -0300

    oi

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBX8JM3te_UlvbVZKeECOYLdY4owQhbfRw",
+    authDomain: "app-agendamento-a0302.firebaseapp.com",
+    projectId: "app-agendamento-a0302",
+    storageBucket: "app-agendamento-a0302.appspot.com",
+    messagingSenderId: "549746932033",
+    appId: "1:549746932033:web:348d5fd29013c0afcd89c0",
+    measurementId: "G-DFLP4G6ZCV"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
